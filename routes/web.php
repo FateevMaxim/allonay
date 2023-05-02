a5b6fa463ce3d65ebb05121b4b45e6ac688f57c3
FateevMaxim <fateev.maxim@gmail.com>
2023-05-02 18:22:12 +0600

    updates

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'welcome']);
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])->name('dashboard');
