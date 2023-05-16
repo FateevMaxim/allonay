c4db14bc3aa8ea715c115cdc5a8e3bce40fb756e
FateevMaxim <fateev.maxim@gmail.com>
2023-05-16 18:23:25 +0600

    updates

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,7 +37,7 @@
                         </div>
                     @endif
 
-                    <div class="bg-[#b6da3a82] p-2 mt-8 mb-4">
+                    {{--<div class="bg-[#b6da3a82] p-2 mt-8 mb-4">
                         <p class="mb-6 text-sm text-dark font-bold">Адрес склада в Китае</p>
                         <p class="mb-6 text-sm text-dark" id="china">@yield( 'chinaaddress' ) <br />
                             <b>({{ \Illuminate\Support\Facades\Auth::user()->login }}){{ \Illuminate\Support\Facades\Auth::user()->name }} </b><br />
@@ -46,12 +46,12 @@
                         </p>
                         <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">@yield( 'address_two' )@yield( 'chinaaddress' )</p>
                         <button onclick="copyText()" class="focus:outline-none text-white bg-[#b6da3a] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Копировать</button>
-                    </div>
+                    </div>--}}
 
-                    <div class="bg-[#fab4b4] p-2 mt-4 mb-4">
+                    {{--<div class="bg-[#fab4b4] p-2 mt-4 mb-4">
                         <p><span>Образец</span></p>
                         <img src="{{asset('images/china.jpg')}}" alt="China">
-                    </div>
+                    </div>--}}
 
                     <hr class="h-px mt-4 bg-gray-200 border-0">
                     @if(\Illuminate\Support\Facades\Auth::user()->type === 'othercity')
@@ -59,12 +59,16 @@
                     @endif
                     <hr class="h-px mt-4 bg-gray-200 border-0">
 
-                    <div class="p-2 mt-4 mb-4">
+                    <div class="p-2 mt-4 mb-2">
                         <div class="grid grid-cols-1 gap-4">
                             <a href="https://t.me/onayall" target="_blank" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">{{ __('Telegram') }}</a>
                         </div>
                     </div>
-
+                    {{--<div class="p-2 mt-2 mb-4">
+                        <div class="grid grid-cols-1 gap-4">
+                            <a href="https://t.me/allonaykzBot?start" target="_blank" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">{{ __('Telegram Bot') }}</a>
+                        </div>
+                    </div>--}}
 
                     <hr class="h-px my-8 bg-gray-200 border-0">
                     <div class="grid grid-cols-2 mt-4 gap-4">
