9544fd7aa9af354d0e758d8ae545252f5455c242
FateevMaxim <fateev.maxim@gmail.com>
2023-07-04 17:44:02 +0600

    updates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,7 +34,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.inputmask.bundle.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.inputmask.min.js') }}"></script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
