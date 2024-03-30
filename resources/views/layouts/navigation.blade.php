0995b1f16f66934386cdc3f43eba65553fa9d524
maximfateev <maxim.f@rocketfirm.com>
2024-03-30 09:36:24 +0500

    copy China

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 
-    <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100">
         <!-- Primary Navigation Menu -->
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -47,10 +47,8 @@
                     <div class="bg-[#b6da3a82] p-2 mt-8 mb-4">
                         <p class="mb-6 text-sm text-dark font-bold">Адрес склада в Китае</p>
                         <p class="mb-6 text-sm text-dark" id="china">@yield( 'chinaaddress' ) <br />
-                            收货人: <b>ALL2023({{ Auth::user()->login }})</b> <br />
-                            电话: <b>@yield( 'address_two' )</b>
                         </p>
-                        <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">@yield( 'address_two' )@yield( 'chinaaddress' )</p>
+                        <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">@yield( 'chinaaddress' )</p>
                         <button onclick="copyText()" class="focus:outline-none text-white bg-[#b6da3a] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Копировать</button>
                     </div>
 
@@ -91,7 +89,7 @@
                 <script>
                     function copyText() {
                         /* Copy text into clipboard */
-                        var chinaaddress = $("#chinaaddress").html();
+                        var chinaaddress = $("#china").html();
                         navigator.clipboard.writeText(chinaaddress);
                     }
 
