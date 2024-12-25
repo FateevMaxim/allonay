ebed1e3366004e94889ae5124e342ec2f9c9d66b
maximfateyev <maxim.f@rocketfirm.com>
2024-12-25 16:45:22 +0700

    Agreement button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -221,6 +221,7 @@
                         </div>
                     @endforeach
                 </div>
+                <script src="{{ asset('/js/flowbite.js') }}"></script>
                 <script type="text/javascript">
                     document.getElementById('toast-success').style.display = 'none';
 
